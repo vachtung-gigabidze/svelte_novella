6d47951172596f233e293ddfb93219ddc9447f0b
dimitrii <dmitry.zyadik@gmail.com>
2025-07-21 16:57:56 +0500

    add rive exapmle

---
+++ src/lib/loadRiveFile.ts
@@ -0,0 +1,17 @@
+// @ts-ignore
+import Rive, { File, RiveCanvas } from "rive-canvas";
+
+export async function loadRiveFile(
+    filePath: string
+): Promise<{
+    rive: RiveCanvas;
+    file: File;
+}> {
+    const req = new Request(filePath);
+    const loadRive = Rive({ locateFile: (file: string) => "file://" + file });
+    const loadFile = fetch(req)
+        .then((res) => res.arrayBuffer())
+        .then((buf) => new Uint8Array(buf));
+    const [rive, file] = await Promise.all([loadRive, loadFile]);
+    return { rive, file: rive.load(file) };
+}
