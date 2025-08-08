e5d7ee8dd02cbc3c50bfa22a681b44cda07264ab
Dmitrii Ziadik <dmitry.zyadik@gmail.com>
2025-08-08 21:17:39 +0300

    try telegrem auth

--- src/App.svelte
+++ src/App.svelte
@@ -1,85 +1,18 @@
 <script lang="ts">
 
     import Header from "./Header.svelte";
-    import Game from "./Game.svelte";
+    import Auth from "./lib/Auth.svelte";
 
-    // let form_state = $state({
-    //         answers: {}, step: 0, error: ''
-    //     }
-    // )
-    //
-    // const QUESTIONS = [
-    //     {
-    //         question: "What you name?",
-    //         id: 'name',
-    //         type: 'text'
-    //     },
-    //     {
-    //         question: "What you birthday?",
-    //         id: 'birthday',
-    //         type: 'date'
-    //     },{
-    //         question: "What you favorite color?",
-    //         id: 'color',
-    //         type: 'color'
-    //     },
-    // ];
-    //
-    // function next_step(id: string){
-    //     if (form_state.answers[id]){
-    //         form_state.step +=1;
-    //         form_state.error = "";
-    //     } else {
-    //         form_state.error = `Please input ${id}`
-    //     }
-    // }
-
-    // $inspect(form_state.step)
-    // $effect(()=> {
-    //     console.log('on mount');
-    //     return ()=> {
-    //
-    //     console.log('on unmount');
-    //     }
-    // });
-    // $effect(()=> {
-    //     console.log('update', form_state.step);
-    // });
+    let name = "User Name"
+   
 
 </script>
-<!--<Header name={form_state.answers.name}/>-->
-<Game />
-<!--<main>-->
-<!--    <div>-->
-<!--        {#if form_state.step >= QUESTIONS.length}-->
-<!--            <p>Спасибо</p>-->
-<!--            {:else}-->
-<!--        <p>Step: {form_state.step + 1}</p>-->
-<!--        {#each QUESTIONS as question , i (question.id)}-->
-<!--            {#if (i === form_state.step)}-->
-<!--                {@render form_step(question)}-->
-<!--            {/if}-->
-<!--        {/each}-->
-<!--        {/if}-->
-<!--    </div>-->
-<!--    <p class="error">-->
-<!--        {form_state.error}-->
-<!--    </p>-->
-<!--    &lt;!&ndash;{JSON.stringify(form_state)}&ndash;&gt;-->
-<!--</main>-->
+<Header name={name}/>
+<Auth/>
 
-<!--{#snippet form_step({question, id, type}: {question:string, id:string, type:string}) }-->
-<!--    <article>-->
 
-<!--        <div>-->
-<!--            <label for={id}>{question}</label>-->
-<!--            <input {type} {id} bind:value={form_state.answers[id]}/>-->
-<!--        </div>-->
-<!--        <button onclick={()=>next_step(id)}>Next</button>-->
-<!--    </article>-->
-<!--{/snippet}-->
-<!--<style>-->
-<!--    .error {-->
-<!--        color: red;-->
-<!--    }-->
-<!--</style>-->
+<style>
+   .error {
+        color: red;
+    }
+</style>
--- src/Header.svelte
+++ src/Header.svelte
@@ -1,22 +1,18 @@
 <script lang="ts">
-    import {createState, MyState} from './state.svelte';
+
 
     let {name}: { name: string } = $props();
 
-    const myState = createState();
-    const myState2 = new MyState();
+    
 </script>
 <div>
 
     <h1>
 
-        {name ? name : "User"} Form
+        {name ? name : "Novella"} Form
     </h1>
-    <button onclick={()=>myState.up()}>{myState.value}</button>
-    <button onclick={()=>myState2.up()}>{myState2.value}</button>
+   
 </div>
 <style>
-    div {
-        background: red;
-    }
+ 
 </style>
