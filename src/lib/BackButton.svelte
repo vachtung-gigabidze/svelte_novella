8a8637906ed7fca66703968b60dbf06bee3dcc65
Dmitrii Ziadik <dmitry.zyadik@gmail.com>
2025-08-15 20:31:46 +0300

    test twa

--- src/lib/BackButton.svelte
+++ src/lib/BackButton.svelte
@@ -1,8 +1,8 @@
 
 <script lang="ts">
   import { onMount, onDestroy } from 'svelte';
-  import { backButton, useSignal, isBackButtonVisible, retrieveLaunchParams } from '@telegram-apps/sdk-svelte';
-  const { initDataRaw, initData } = retrieveLaunchParams();
+  import { backButton, useSignal, isBackButtonVisible, initData } from '@telegram-apps/sdk-svelte';
+ 
   const isVisible = useSignal(backButton.isVisible);
 
   let info = $derived(`The button is, ${isBackButtonVisible() ? 'visible' : 'invisible'}`);
