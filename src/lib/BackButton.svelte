62352442efc5082f006f4d7502a8aa8384a7acaf
Dmitrii Ziadik <dmitry.zyadik@gmail.com>
2025-08-15 16:59:25 +0300

    test twa

---
+++ src/lib/BackButton.svelte
@@ -0,0 +1,19 @@
+/**
+ * Component which controls the Back Button visibility.
+ */
+<script lang="ts">
+  import { onMount, onDestroy } from 'svelte';
+  import { backButton, useSignal } from '@telegram-apps/sdk-svelte';
+
+  const isVisible = useSignal(backButton.isVisible);
+
+  $: console.log('The button is', isVisible.value ? 'visible' : 'invisible')
+
+  onMount(() => {
+    backButton.show();
+  });
+
+  onDestroy(() => {
+    backButton.hide();
+  });
+</script>
