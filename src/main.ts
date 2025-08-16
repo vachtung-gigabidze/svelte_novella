1325cdc741ff06beba875ce9aa1284deadcfe9b1
Dmitrii Ziadik <dmitry.zyadik@gmail.com>
2025-08-16 23:10:56 +0300

    test twa

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,23 @@
 import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
+import { init, miniApp } from '@telegram-apps/sdk-svelte';
 
+const initializeTelegramSDK = async () => {
+  try {
+    await init();
+    
+    if (miniApp?.ready) {
+      await miniApp.ready();
+      console.log('Mini App готово');
+    }
+  } catch (error) {
+    console.error('Ошибка инициализации:', error);
+  }
+};
+
+// Initialize Telegram SDK
+initializeTelegramSDK();
 const app = mount(App, {
   target: document.getElementById('app')!,
 })
