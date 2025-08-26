fd51b00a26728e0b6b9187c35480ebcadd757b0d
dimitrii <dmitry.zyadik@gmail.com>
2025-08-26 16:16:34 +0500

    test supabase

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,18 @@
-<script>
+<script lang="ts">
 
 import App from "./novella/App.svelte";
-</script>
+import { onMount } from 'svelte';
 
+let userId: string | null = null;
+
+onMount(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    userId = urlParams.get('id');
+
+});
+
+</script>
+<p>User ID: {userId}</p>
 <App></App>
 <style>
 
